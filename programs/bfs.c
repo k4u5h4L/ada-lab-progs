e3c144bebbea74f62a2a9bcdd6c3eafb91df8c6a
Kaushal <kaushal.v.bhat@gmail.com>
2020-07-19 19:18:29 +0530

    update bfs

--- programs/bfs.c
+++ programs/bfs.c
@@ -4,34 +4,41 @@
 #include <time.h>
 #include <sys/resource.h>
 
-int a[50][50], q[50], f = 1, r = -1, visited[50];
+int q[30], f = 0, r = -1;
+int visited[20];
 
-int bfs(int n) {
-    int i, j;
-    int v = 0;
+void bfs(int v, int n, int a[][n]) {
+    int i;
 
     for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            if (a[v][j] && !visited[i]) {
-                q[++r] = j;
-            }
-            if (f <= r) {
-                visited[q[f]] = 1;
-                printf("%d  ", f++);
-            }
+        if (a[v][i] && !visited[i]) {
+            q[++r] = i;
         }
     }
-    printf("\n");
-}
 
+    while (f <= r) {
+        visited[q[f]] = 1;
+
+        printf("Visited node index: %d\t", q[f++]);
+    }
+
+    f = 0;
+    r = -1;
+
+    if (v < n - 1) {
+        bfs(v + 1, n, a);
+    }
+}
 
 int main(void) {
     struct timeval tv1, tv2;
     int i, j, n;
     struct rusage r_usage;
     
-    printf("Enter row size\n");
+    printf("Enter number of nodes\n");
     scanf("%d", &n);
+
+    int a[n][n];
     
     printf("Enter the matrix elements\n");
     for (i = 0; i < n; i++) {
@@ -40,8 +47,17 @@ int main(void) {
         }
     }
 
+    for (i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+
+    printf("Enter index for source node\n");
+    scanf("%d", &i);
+
+    printf("Source node is %d\n", i);
+
     gettimeofday(&tv1, NULL);
-    bfs(n);
+    bfs(i, n, a);
     gettimeofday(&tv2, NULL);
 
     printf("Time of bfs = %.2f microseconds\n", (double)(tv2.tv_usec - tv1.tv_usec));
